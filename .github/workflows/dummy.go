6f4c3d3abfe6804397939b34d2ef2703c4d7f009
Rafael Vigo <rvigo07+github@gmail.com>
2022-07-13 01:06:23 -0300

    feat: new println

--- .github/workflows/dummy.go
+++ .github/workflows/dummy.go
@@ -1,6 +1,11 @@
 package main
+import (
+	"fmt"
+)
 
 func main() {
 	// this is a workaround to release rust binaries with goreleaser
 	// ref: https://jondot.medium.com/shipping-rust-binaries-with-goreleaser-d5aa42a46be0s
+	
+	fmt.Println("this is from dummy")
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
 fn main() {
     println!("Hello, world!");
+    print!("v1.6.6")
 }
