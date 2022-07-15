47f8caa1b5204217cfcfe617d1467b82e3dc96ab
Rafael Vigo <rvigo07+github@gmail.com>
2022-07-15 01:07:35 -0300

    ci: includes debug options for post_build script

--- .github/workflows/post_build.sh
+++ .github/workflows/post_build.sh
@@ -10,24 +10,31 @@ ARM64_LINUX=target/aarch64-unknown-linux-gnu/release/hello
 if [ -f "$x86_64_DARWIN" ]; then
     echo "replacing x86_64 apple bin"
     
-    rm -f dist/hello_darwin_amd64_v1/hello \
-    && mv $x86_64_DARWIN dist/hello_darwin_amd64_v1/hello
+    error=$(rm -f dist/hello_darwin_amd64_v1/hello \
+    && mv $x86_64_DARWIN dist/hello_darwin_amd64_v1/hello)
 fi
 if [ -f "$x86_64_LINUX" ]; then
     echo "replacing x86_64 linux bin"
     
-    rm -f dist/hello_linux_amd64_v1/hello \
-    && mv $x86_64_LINUX dist/hello_linux_amd64_v1/hello
+    error=$(rm -f dist/hello_linux_amd64_v1/hello \
+    && mv $x86_64_LINUX dist/hello_linux_amd64_v1/hello 2>&1 )
 fi
 if [ -f "$ARM64_DARWIN" ]; then
     echo "replacing arm64 apple bin"
     
-    rm -f dist/hello_darwin_arm64/hello \
-    && mv $ARM64_DARWIN dist/hello_darwin_arm64/hello
+    error=$(rm -f dist/hello_darwin_arm64/hello \
+    && mv $ARM64_DARWIN dist/hello_darwin_arm64/hello)
 fi
 if [ -f "$ARM64_LINUX" ]; then
     echo "replacing arm64 linux bin"
     
-    rm -f dist/hello_linux_arm64/hello \
-    && mv $ARM64_LINUX dist/hello_linux_arm64/hello
+    error=$(rm -f dist/hello_linux_arm64/hello \
+    && mv $ARM64_LINUX dist/hello_linux_arm64/hello)
+fi
+
+touch result.done
+if [ -z "$error" ]; then
+    echo "done!" >> result.done
+else
+    echo $error >> result.done
 fi
