6029e4d36b69cd91fe27b4359a16424bdca1c013
Rafael Vigo <rvigo07+github@gmail.com>
2022-08-31 22:16:09 -0300

    test: includes test scenario

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", get_message());
     println!("v1.10.1");
+}
+
+fn get_message<'a>() -> &'a str {
+    "Hello, world!"
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn should_get_message() {
+        assert_eq!(get_message(), "Hello, world!")
+    }
 }
