8b8988858b136d0578cbaccb26d4ca02f12834a4
Rafael Vigo <rvigo07+github@gmail.com>
2022-09-10 05:48:40 +0000

    docs: improves homebrew caveats message

--- completions/zsh/hello.zsh
+++ completions/zsh/hello.zsh
@@ -1,8 +1,7 @@
 #compdef _hello hello
-autoload -U is-at-least
 
-function _hello {
-    _arguments - C \
+_hello() {
+    _arguments -s -S -C \
     "-h[Show help]" \
     "1: :(option1 option2)" \
     "*::arg:->args"
